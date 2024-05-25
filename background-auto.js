28ae9c0c18160a1f839d375cb0a8f6a56531b716
krishna-tx <krishna.ramesh.tx@gmail.com>
2024-05-24 19:18:05 -0500

    better shorts removal method

--- background-auto.js
+++ background-auto.js
@@ -6,28 +6,30 @@ const shortsURL = "https://www.youtube.com/shorts";
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if(tab.url && tab.url.includes(homeURL)) { // check if url exists and is a youtube url
-        // check if page is a video page => insert css
-        if(tab.url.includes(watchURL)) {
+        await chrome.scripting.insertCSS({
+            files: ["shorts-removal.css"],
+            target: {tabId: tab.id}
+        });
+
+        // check if page is a home page => hide recommendations
+        if(tab.url == homeURL || tab.url.includes(reloadedHomeURL)) {
+            await chrome.scripting.insertCSS({
+                files: ["home-page.css"],
+                target: {tabId: tab.id}
+            });
+        }
+        // check if page is a video page => hide related videos/shorts
+        else if(tab.url.includes(watchURL)) {
             await chrome.scripting.insertCSS({
                 files: ["watch-page.css"],
                 target: {tabId: tab.id}
             });
         }
+        // check if page is a results page => hide shorts
         else if(tab.url.includes(resultsURL)) {
             console.log("results page");
             await chrome.scripting.insertCSS({
                 files: ["results-page.css"],
-                target: {tabId: tab.id}
-            });
-        }
-        // check if page is a shorts page => redirect to home page
-        else if(tab.url.includes(shortsURL)) {
-            chrome.tabs.update({url: homeURL});
-        }
-        // check if page is a home page => insert css
-        else if(tab.url == homeURL || tab.url.includes(reloadedHomeURL)) {
-            await chrome.scripting.insertCSS({
-                files: ["home-page.css"],
                 target: {tabId: tab.id}
             });
         }

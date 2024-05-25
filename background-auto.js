cf0b0a11a4e514f7f460cb7be01e60a2827b0a82
krishna-tx <krishna.ramesh.tx@gmail.com>
2024-05-24 20:17:26 -0500

    reorganization of remove css

--- background-auto.js
+++ background-auto.js
@@ -2,45 +2,80 @@ const watchURL = "https://www.youtube.com/watch";
 const resultsURL = "https://www.youtube.com/results";
 const homeURL = "https://www.youtube.com/";
 const reloadedHomeURL = "https://www.youtube.com/?";
-const shortsURL = "https://www.youtube.com/shorts";
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if(tab.url && tab.url.includes(homeURL)) { // check if url exists and is a youtube url
-        await chrome.scripting.insertCSS({
+        // remove shorts
+        chrome.scripting.insertCSS({
             files: ["shorts-removal.css"],
             target: {tabId: tab.id}
         });
-
         // check if page is a home page => hide recommendations
         if(tab.url == homeURL || tab.url.includes(reloadedHomeURL)) {
-            await chrome.scripting.insertCSS({
+            // remove css
+            chrome.scripting.removeCSS({
+                files: ["watch-page.css"],
+                target: {tabId: tab.id}
+            });
+            chrome.scripting.removeCSS({
+                files: ["results-page.css"],
+                target: {tabId: tab.id}
+            });
+
+            // insert css
+            chrome.scripting.insertCSS({
                 files: ["home-page.css"],
                 target: {tabId: tab.id}
             });
         }
         // check if page is a video page => hide related videos/shorts
         else if(tab.url.includes(watchURL)) {
-            await chrome.scripting.insertCSS({
+            // remove css
+            chrome.scripting.removeCSS({
+                files: ["home-page.css"],
+                target: {tabId: tab.id}
+            });
+            chrome.scripting.removeCSS({
+                files: ["results-page.css"],
+                target: {tabId: tab.id}
+            });
+
+            // insert css
+            chrome.scripting.insertCSS({
                 files: ["watch-page.css"],
                 target: {tabId: tab.id}
             });
         }
         // check if page is a results page => hide shorts
         else if(tab.url.includes(resultsURL)) {
-            console.log("results page");
-            await chrome.scripting.insertCSS({
+            // remove css
+            chrome.scripting.removeCSS({
+                files: ["home-page.css"],
+                target: {tabId: tab.id}
+            });
+            chrome.scripting.removeCSS({
+                files: ["watch-page.css"],
+                target: {tabId: tab.id}
+            });
+
+            // insert css
+            chrome.scripting.insertCSS({
                 files: ["results-page.css"],
                 target: {tabId: tab.id}
             });
         }
         // otherwise => remove css
         else {
-            await chrome.scripting.removeCSS({
-                files: ["watch-page.css"],
+            chrome.scripting.removeCSS({
+                files: ["home-page.css"],
                 target: {tabId: tab.id}
             });
-            await chrome.scripting.removeCSS({
-                files: ["home-page.css"],
+            chrome.scripting.removeCSS({
+                files: ["results-page.css"],
+                target: {tabId: tab.id}
+            });
+            chrome.scripting.removeCSS({
+                files: ["watch-page.css"],
                 target: {tabId: tab.id}
             });
         }
